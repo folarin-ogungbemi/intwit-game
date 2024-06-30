fa6d410967f206be2810e0098b0453b2b4b359e2
folarin-ogungbemi <ftogungbemi@gmail.com>
2024-06-30 20:08:49 +0200

    populate data

--- assets/scripts/base.js
+++ assets/scripts/base.js
@@ -1,5 +1,26 @@
 fetch('assets/data/data.json')
-    .then(response => response.json())
-    .then(data => {
-        console.log(data)
-    })
+    .then(response => {
+        if (!response.ok){
+            throw new Error('Network issues')
+        }
+        else {
+            response.json()
+            .then(data => {
+                const mainContent = document.getElementsByTagName('main')[0];
+                data.map(
+                    (item, index) => {
+                        const divElement = document.createElement('div');
+                        divElement.innerHTML = `
+                        <p><span>${index+1}.</span> ${item.question}</p>
+                        <ul class="list-items">
+                            <li><span>A.</span> ${item.a}</li>
+                            <li><span>B.</span> ${item.b}</li>
+                            <li><span>C.</span> ${item.c}</li>
+                            <li><span>D.</span> ${item.d}</li>
+                        </ul>`;
+                        mainContent.appendChild(divElement);
+                });
+            })
+            .catch(error => {console.log("Error fetching data!: ", error)});
+            }
+        })
